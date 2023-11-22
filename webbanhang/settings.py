9eba5c88d408bfcdd912dad0d14fc1e99e26cb1a
Nguyễn Văn Minh <102002130+Minh072k3@users.noreply.github.com>
2023-11-22 15:57:51 +0700

    Update settings.py

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -118,16 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-]
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "app/static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 
 MEDIA_URL = '/images/'
